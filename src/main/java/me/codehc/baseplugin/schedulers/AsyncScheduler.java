4bed74e918cc3f0050983905b773f17c5900de7f
Orlaand-dev <orlando.lacrue.cg@scienceandtech.org>
2021-05-05 10:40:00 -0600

    Prevent deadlocks

--- src/main/java/me/codehc/baseplugin/schedulers/AsyncScheduler.java
+++ src/main/java/me/codehc/baseplugin/schedulers/AsyncScheduler.java
@@ -8,15 +8,15 @@ import java.util.function.Consumer;
 public class AsyncScheduler extends Scheduler {
     public AsyncScheduler() {}
 
-    public void runTask(Consumer<BukkitRunnable> task) {
+    public synchronized void runTask(Consumer<BukkitRunnable> task) {
         getRunnable(task).runTaskAsynchronously(BasePlugin.INSTANCE);
     }
 
-    public void runTaskLater(Consumer<BukkitRunnable> task, long delay) {
+    public synchronized void runTaskLater(Consumer<BukkitRunnable> task, long delay) {
         getRunnable(task).runTaskLaterAsynchronously(BasePlugin.INSTANCE, delay);
     }
 
-    public void runTaskTimer(Consumer<BukkitRunnable> task, long delay, long period) {
+    public synchronized void runTaskTimer(Consumer<BukkitRunnable> task, long delay, long period) {
         getRunnable(task).runTaskTimerAsynchronously(BasePlugin.INSTANCE, delay, period);
     }
 
